6549614cfafb9367c894acd672f8b8d7c236d017
Felix Omondi <felomondi2@gmail.com>
2024-07-01 22:06:50 +0000

    add the API code

---
+++ youtube.py
@@ -0,0 +1,30 @@
+import os
+from googleapiclient.discovery import build
+
+# Replace with your API key
+API_KEY = 'AIzaSyBXPzyA9-6_eztFQs8TQqD5hWRQ2eEOmRo'
+
+# Create a service object
+youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+def search_videos(query, max_results=5):
+    #Use the search.list method to retrieve results matching the specified query term
+    request = youtube.search().list(
+        part="snippet",
+        q=query,
+        type="video",
+        maxResults=max_results
+    )
+
+    response = request.execute()
+
+    for item in response['items']:
+        title = item['snippet']['title']
+        video_id = item['id']['videoId']
+        url = f'https://www.youtube.com/watch?v={video_id}'
+        print(f'Title: {title}')
+        print(f'URL: {url}')
+        print('---')
+
+if __name__ == '__main__':
+    search_videos('Python programming', max_results=5)
